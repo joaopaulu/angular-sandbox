5f9a4f5f0002b6345e9a13771ff55a55ee4f8f5b
joaopaulu <jptick@gmail.com>
2020-11-30 21:53:13 -0300

    mgModule and template

--- curso-angular2/primeiro-projeto/src/app/app.module.ts
+++ curso-angular2/primeiro-projeto/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuSegundoComponent } from './meu-segundo/meu-segundo.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [AppComponent, MeuPrimeiroComponent, MeuSegundoComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, CursosModule],
   providers: [],
   bootstrap: [AppComponent],
 })
