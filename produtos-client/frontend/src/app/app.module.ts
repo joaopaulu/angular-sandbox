89c41856e54b3873878ac434fc0765eb595c64b8
joaopaulu <jptick@gmail.com>
2020-12-20 15:34:15 -0300

    Create product component and setting navigation

--- produtos-client/frontend/src/app/app.module.ts
+++ produtos-client/frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
+import { ProductComponent } from './views/product/product.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { HomeComponent } from './views/home/home.component';
     FooterComponent,
     NavComponent,
     HomeComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
