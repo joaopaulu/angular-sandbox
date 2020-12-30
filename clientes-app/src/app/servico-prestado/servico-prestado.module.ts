84db1c8492c53a4cf4d547f269de655d6d27290a
João Paulo Lima <jptick@gmail.com>
2020-12-29 22:33:11 -0300

    Update Form Servicos Prestados

--- clientes-app/src/app/servico-prestado/servico-prestado.module.ts
+++ clientes-app/src/app/servico-prestado/servico-prestado.module.ts
@@ -3,17 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { ServicoPrestadoRoutingModule } from './servico-prestado-routing.module';
 import { ServicoPrestadoListaComponent } from './servico-prestado-lista/servico-prestado-lista.component';
+import { ServicoPrestadoFormComponent } from './servico-prestado-form/servico-prestado-form.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [ServicoPrestadoListaComponent],
+  declarations: [ServicoPrestadoListaComponent, ServicoPrestadoFormComponent],
   imports: [
     CommonModule,
     ServicoPrestadoRoutingModule,
     FormsModule,
     RouterModule,
   ],
-  exports: [ServicoPrestadoListaComponent],
+  exports: [ServicoPrestadoListaComponent, ServicoPrestadoFormComponent],
 })
 export class ServicoPrestadoModule {}
